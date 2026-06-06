19bd3bb8e55ee5bc8e69fc69eb9225362046a3fd
David Lee <gongchoong33@gmail.com>
2026-06-06 11:45:47 -0500

    Initial Commit

---
+++ nike_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nike_swiftui
+//
+//  Created by davidlee on 6/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nike_swiftui/nike_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nike_swiftuiApp.swift
+//  nike_swiftui
+//
+//  Created by davidlee on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct nike_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
